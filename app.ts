7f312ebc1c2d501eb6e4a89eef2614b77887b2f8
TJ <tomaz.jejcic@bridqe.com>
2016-11-20 14:04:32 +0800

    reddit-article template error

--- app.ts
+++ app.ts
@@ -3,25 +3,105 @@ import { BrowserModule } from '@angular/platform-browser';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 @Component({
-    selector: 'hello-world',
+    selector: 'reddit-article',
+    host: {
+        class: 'row'
+    },
     template: `
-    <div>
-        Hello world
+    <div class="four wide column center alligned votes">
+        <div class="ui statistic">
+            <div class="value">
+                {{ votes }}
+            </div>
+            <div class="label">
+                Points
+            </div>
+        </div>
+    </div>
+    <div class="twelve wide column">
+        <a class="ui large header" href="{{ link }}">
+            {{ title }}
+        </a>
+        <ul class="ui big horizontal list voters">
+            <li class="item">
+                <a href (click)="voteUp()">
+                    <i class="arrow up icon"></i>
+                        upvote
+                </a>
+            </li>
+            <li class="item">
+                <a href (click)="voteDown()">
+                    <i class="arrow up icon"></i>
+                        downvote
+                </a>
+            </li>
+        </ul>
     </div>
     `
 })
+class ArticleComponent {
+    votes: number;
+    title: string;
+    link: string;
 
-class HelloWorld {
+    constructor() {
+        this.title = 'Angular 2';
+        this.link = 'http://angular.io';
+        this.votes = 10;
+    }
 
+    voteUp() {
+        this.votes += 1;
+    }
+
+    voteDown() {
+        this.votes -= 1;
+    }
+
+}
+
+@Component({
+    selector: 'reddit',
+    template: `
+    <div>
+    <form class="ui large form segment">
+        <h3 class="ui header">Add a link</h3>
+        <div class="field">
+            <label for="title">Title:</label>
+            <input name="title" #newtitle>
+        </div>
+        <div class="field">
+            <label for="link">Link:</label>
+            <input name="link" #newlink>
+        </div>
+        <button (click)="addArticle(newtitle, newlink)"
+                class="ui positive right floated button">
+            Submit link
+        </button>
+    </form>
+    <div class="ui grid posts">
+        <reddit-article></reddit-article>
+    </div>
+    </div>
+    `
+})
+class RedditApp {
+    addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+        console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+        return false;
+    }
 }
 
 @NgModule({
-    declarations: [HelloWorld],
+    declarations: [
+        RedditApp,
+        ArticleComponent
+    ],
     imports: [BrowserModule],
-    bootstrap: [HelloWorld]
+    bootstrap: [RedditApp]
 })
 
-class HelloWorldAppModule {
+class RedditAppModule {
 
 }
-platformBrowserDynamic().bootstrapModule(HelloWorldAppModule);
+platformBrowserDynamic().bootstrapModule(RedditAppModule);
